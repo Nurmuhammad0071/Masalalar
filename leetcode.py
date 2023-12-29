00ea69d70d844b2eabd722f559f46da90ec735bb
Nurmuhammad0071 <87298038+Nurmuhammad0071@users.noreply.github.com>
2023-12-29 16:42:47 +0500

    Update leetcode.py
    feat: leetcode 58

--- leetcode.py
+++ leetcode.py
@@ -5,3 +5,9 @@ class Solution:
         for num in nums:
          result ^= num
         return result
+#58. Length of Last Word
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+       text = s.split()
+       t =  text[-1]
+       return len(t)
